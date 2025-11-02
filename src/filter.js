200f9de7205593c62deba33c1a128a972d0d2d2a
haileebuij <hailee.bui@wellmo.com>
2025-11-02 17:06:36 +0200

    Fix test

--- src/filter.js
+++ src/filter.js
@@ -26,7 +26,7 @@ function filter(array, predicate) {
   let resIndex = 0
   const length = array == null ? 0 : array.length
   // const result = [[]]
-  const result = [[]] // for pipeline testing
+  const result = [] // for pipeline testing
 
 
   while (++index < length) {
